6854f40e87a35c04f1f9a1b0a277db76f7800a4b
Carlo kajyr <carlo.panzi@gmail.com>
2014-09-30 22:51:23 +0200

    some initial work

--- dist/kanbanize.js
+++ dist/kanbanize.js
@@ -6,11 +6,49 @@
 
     function KanbanizeJS(options) {
       var domain;
-      this.key = options.key, domain = options.domain, this.user = options.user, this.password = options.password;
+      this.email = options.email, this.password = options.password, domain = options.domain;
       domain = domain != null ? domain : '';
       this.kanbanize_url = "http://'" + domain + "." + this.BASE_URL;
       extend({}, {});
     }
+
+    KanbanizeJS.prototype.call = function(apiCall) {
+      var call;
+      call = this.executeCall(apiCall);
+      return call.getResponseDecoded();
+    };
+
+    KanbanizeJS.prototype.executeCall = function(apiCall) {
+      var $l, key, params, url, val;
+      if (apiCall["function"] !== 'login' && (this.apikey == null)) {
+        $l = this.login();
+        this.apikey = $l['apikey'];
+      }
+      url = this.kanbanize_url + "/" + apiCall["function"];
+      params = ((function() {
+        var _ref, _results;
+        _ref = apiCall.data;
+        _results = [];
+        for (key in _ref) {
+          val = _ref[key];
+          _results.push(key + '/' + val);
+        }
+        return _results;
+      })()).join('/');
+      return $.ajax(url + params);
+    };
+
+    KanbanizeJS.prototype.login = function() {
+      var call;
+      call = {
+        "function": 'login',
+        data: {
+          email: this.email,
+          password: this.password
+        }
+      };
+      return this.call(call);
+    };
 
     return KanbanizeJS;
 
--- src/kanbanize.coffee
+++ src/kanbanize.coffee
@@ -2,8 +2,41 @@ class KanbanizeJS
 	BASE_URL: 'kanbanize.com/index.php/api/kanbanize';
 
 	constructor: (options) -> 
-		{@key, domain, @user, @password} = options
+		{ @email, @password, domain } = options
 		domain = domain ? ''
 		@kanbanize_url = "http://'#{domain}.#{@BASE_URL}"
 
 		extend {}, {}
+
+	call: (apiCall) ->
+
+		call = @executeCall(apiCall)
+		
+
+		return call.getResponseDecoded();
+	
+	executeCall: (apiCall) ->
+		
+		if apiCall.function != 'login' && !@apikey?
+			$l = @login()
+			@apikey = $l['apikey']
+
+		url = @kanbanize_url + "/" + apiCall.function
+
+		params = (for key, val of apiCall.data
+			key+'/'+val).join '/'
+
+
+		$.ajax url + params
+
+		
+
+	login: () ->
+		call = {
+			function: 'login'
+			data:
+				email: @email
+				password: @password
+		}
+	
+		return @call(call);
